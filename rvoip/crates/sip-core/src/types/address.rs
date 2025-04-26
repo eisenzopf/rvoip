ec701232496d4f6e28277b07475145b2b7f10156
eisenzopf <ironbraid74@gmail.com>
2025-04-25 21:16:31 -0700

    Fixing address parsing

--- rvoip/crates/sip-core/src/types/address.rs
+++ rvoip/crates/sip-core/src/types/address.rs
@@ -66,7 +66,7 @@ impl fmt::Display for Address {
 
         // Write parameters
         for param in &self.params {
-            write!(f, "{}", param)?;
+            write!(f, ";{}", param)?;
         }
 
         Ok(())
