9fd29e670f055c46d54be1382eac42ed5cc932e1
eisenzopf <ironbraid74@gmail.com>
2025-07-24 16:28:47 -0700

    Fixing lsp quantizer.

--- crates/codec-core/src/codecs/g729a-new/tests/lsp_quantizer/rust_test.rs
+++ crates/codec-core/src/codecs/g729a-new/tests/lsp_quantizer/rust_test.rs
@@ -1,12 +1,14 @@
+
 use g729a_new::encoder::lsp_quantizer::az_lsp;
 use g729a_new::encoder::lspvq::LspQuantizer;
+use g729a_new::common::basic_operators::Word16;
 
 #[test]
 fn test_lsp_quantizer() {
     let a = [4096, -4174, 1, 17, 12, 13, 13, 13, 11, 9, 103];
     let mut lsp = [0; 10];
     let mut lsp_q = [0; 10];
-    let mut ana = [0; 2];
+    let mut ana: [Word16; 2] = [0; 2];
     let old_lsp = [0; 10];
 
     println!("rust_function_name,rust_output");
